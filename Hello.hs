ff7887506e23be6c3caf29cd93f35188d9b59d1e
Even Lislebø <e.lislebo@gmail.com>
2014-12-11 15:10:40 +0100

    Prompt - read - print

--- Hello.hs
+++ Hello.hs
@@ -3,5 +3,6 @@ import System.Environment
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn ("arg1 + arg2 = " ++ show ((read $ args!!0) + (read $ args!!1)))
+  putStrLn "Enter name:"
+  name <- getLine
+  putStrLn $ "Name: " ++ name
